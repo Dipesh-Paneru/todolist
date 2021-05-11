122d0b23399f4219258227c8d7e4823d38ccf73f
dipesh paneru <dipeshpaneru54@gmail.com>
2021-05-11 11:48:33 +0545

    creating user interface

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
+import React, {useState} from "react"
 import './App.css';
+// importing components
+import Form from "./components/Form"
+import TodoList from "./components/TodoList"
+
 
 function App() {
+    const [inputText, setInputText]= useState("")
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+     <h1>dipesh's todo list</h1>
       </header>
+     <Form setInputText={setInputText}/>
+     <TodoList/>
     </div>
   );
 }
 
 export default App;
+
