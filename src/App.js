07b36c4d9ffd0530721478142a812638d30657f9
dipesh paneru <dipeshpaneru54@gmail.com>
2021-05-14 20:28:13 +0545

    added todo functionality

--- src/App.js
+++ src/App.js
@@ -7,13 +7,14 @@ import TodoList from "./components/TodoList"
 
 function App() {
     const [inputText, setInputText]= useState("")
+    const [todos, setTodos]= useState([])
   return (
     <div className="App">
       <header>
      <h1>dipesh's todo list</h1>
       </header>
-     <Form setInputText={setInputText}/>
-     <TodoList/>
+     <Form inputText={inputText} todos={todos} setTodos={setTodos} setInputText={setInputText}/>
+     <TodoList  setTodos={setTodos} todos={todos}/>
     </div>
   );
 }
